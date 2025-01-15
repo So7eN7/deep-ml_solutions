aca7cabb170f079064e9dbef1d7d8c18f3994f57
so7en7 <soorenaaryan7@protonmail.com>
2025-01-15 13:11:25 +0330

    52

---
+++ Implement Recall Metric in Binary Classification.py
@@ -0,0 +1,6 @@
+import numpy as np
+def recall(y_true, y_pred):
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    recall = tp / (tp + fn)
+    return round(recall)
